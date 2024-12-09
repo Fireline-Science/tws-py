26efbae7c54219b68b56fc20264fd6af4c9006d9
Sean Schaefer <sean.schaefer2@gmail.com>
2024-12-09 15:59:29 -0700

    ci: add pyright check to pull request workflow

--- tws/_sync/client.py
+++ tws/_sync/client.py
@@ -1,13 +1,20 @@
 from supabase import create_client as create_supabase_client
+from supabase import Client as SyncSupabaseClient, ClientOptions
 
 from tws.base.client import TWSClient, ClientException
 
 
 class SyncClient(TWSClient):
+    api_client_options: ClientOptions
+    api_client: SyncSupabaseClient
+
     @classmethod
     def create(cls, public_key: str, secret_key: str, api_url: str):
         self = cls(public_key, secret_key, api_url)
         try:
+            self.api_client_options = ClientOptions(
+                headers={"Authorization": secret_key}
+            )
             self.api_client = create_supabase_client(
                 api_url, public_key, self.api_client_options
             )
